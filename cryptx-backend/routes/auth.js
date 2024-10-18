489980273a0f16622a7878687ba2f984f6b73021
Refilwe06 <refilwemotseo2@gmail.com>
2024-10-18 18:40:31 +0200

    feat: added auth route (API) for signing in

--- cryptx-backend/routes/auth.js
+++ cryptx-backend/routes/auth.js
@@ -54,4 +54,40 @@ router.post("/sign-up", (req, res) => {
     });
 });
 
+//  Log existing user in
+router.post("/sign-in", (req, res) => {
+    const { username, password } = req.body;
+
+    // fetch user from my db using the provided username
+    const sql = "SELECT * FROM cryptx_users WHERE username = ?";
+
+    db.query(sql, [username], (err, data) => {
+        // return db error back to my frontend if it exists
+        if (err) return res.status(500).json({ err: err.message });
+
+        if (data.length > 0) {
+            const [user] = data;
+            // compare the hashed and provided passwords 
+            bcrypt.compare(password, user.password, (err, passwordMatches) => {
+                if (err) return res.status(400).json({ err: "Incorrect credentials" });
+                if (passwordMatches) {
+                    // sign the user_id and create a token for the logged in user
+                    const token = jwt.sign({ user_id: user.user_id }, process.env.JWT_SECRET, {
+                        expiresIn: "1d",
+                    });
+                    delete user.password; // Don't send the password back for security
+
+                    // send user object back to my frontend upon successful login
+                    return res.status(200).json({ msg: "Login successful!", user, token });
+                } else {
+                    // send back error if the password provided did not match the hashed one
+                    return res.status(400).json({ err: "Incorrect credentials" });
+                }
+            });
+        } else {
+            return res.status(400).json({ err: "Username not found" });
+        }
+    });
+});
+
 module.exports = router;

251b83894baa9a6607c88b9103893158bd96a67e
Refilwe06 <refilwemotseo2@gmail.com>
2024-10-20 02:45:36 +0200

    refactor: removed test data

--- cryptx-frontend/src/components/Transactions/Transactions.js
+++ cryptx-frontend/src/components/Transactions/Transactions.js
@@ -1,37 +1,9 @@
+import { useState } from 'react';
 import TransactionsItem from '../TransactionsItem/TransactionsItem';
 import './Transactions.css';
 
 const Transactions = () => {
-    const transactions = [
-        {
-            coin: 'Ethereum',
-            status: 'Received',
-            price: '24102',
-            date: 'Today, 19:30',
-            isProfit: true
-        },
-        {
-            coin: 'Bitcoin',
-            status: 'Buy',
-            price: '4157',
-            date: 'Today, 14:32',
-            isProfit: false
-        },
-        {
-            coin: 'Bitcoin',
-            status: 'Buy',
-            price: '64784',
-            date: 'Today, 13:50',
-            isProfit: false
-        },
-        {
-            coin: 'Litecoin',
-            status: 'Buy',
-            price: '14265',
-            date: 'Today, 09:38',
-            isProfit: false
-        },
-    ]
+    const [transactions, setTransactions] = useState([]);
     return (
         <div className="transactions">
             {

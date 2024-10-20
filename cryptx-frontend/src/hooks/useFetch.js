5f0295fade247e1ff2180c6cf12d7701c6a2b645
Refilwe06 <refilwemotseo2@gmail.com>
2024-10-20 23:39:10 +0200

    feat: added useFetch hook

---
+++ cryptx-frontend/src/hooks/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const response = await fetch(url);
+                if (!response.ok) {
+                    const errorData = await response.json();
+                    throw new Error(errorData?.err || `Error: ${response.status}`);
+                }
+                const result = await response.json();
+                setData(result);
+            } catch (err) {
+                console.err(err)
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [url]);
+
+    return { data, loading, error };
+};
+
+export default useFetch;

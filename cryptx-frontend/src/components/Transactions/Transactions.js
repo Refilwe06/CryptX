923a32cbce28af60002498d2c8529c5aae7f3103
Refilwe06 <refilwemotseo2@gmail.com>
2024-10-20 23:39:46 +0200

    feat: integrated transactions using my own API

--- cryptx-frontend/src/components/Transactions/Transactions.js
+++ cryptx-frontend/src/components/Transactions/Transactions.js
@@ -1,15 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TransactionsItem from '../TransactionsItem/TransactionsItem';
 import './Transactions.css';
+import useFetch from '../../hooks/useFetch';
 
 const Transactions = () => {
     const [transactions, setTransactions] = useState([]);
+    const { data, loading, error } = useFetch('http://localhost:5000/crypto/get-transactions');
+
+    useEffect(() => {
+        if (data) setTransactions(data?.transactions)
+
+    }, [data])
     return (
-        <div className="transactions">
+        <>
             {
-                transactions.map((trans, index) => <TransactionsItem key={index} {...trans} />)
+                loading ? <p>Loading...</p>
+                    :
+                    error ? <p>{error}.</p>
+                        :
+                        <div className="transactions">
+                            {
+                                transactions?.map((trans, index) => <TransactionsItem key={index} {...trans} />)
+                            }
+                        </div>
             }
-        </div>
+        </>
     )
 }
 

b1c969ac40a3315ab5213591881c05f8310214f1
Refilwe06 <refilwemotseo2@gmail.com>
2024-10-21 02:12:56 +0200

    feat: complete Dashboard Component

--- cryptx-frontend/src/components/Dashboard/Dashboard.js
+++ cryptx-frontend/src/components/Dashboard/Dashboard.js
@@ -8,7 +8,7 @@ import Transactions from '../Transactions/Transactions';
 import './Dashboard.css';
 
 const Dashboard = () => {
-    const { data, loading, error } = useFetch('http://localhost:5000/crypto/fetch-coin-data');
+    const { data } = useFetch('http://localhost:5000/crypto/fetch-coin-data');
     const [coinData, setCoinData] = useState([]);
 
     useEffect(() => {
@@ -33,12 +33,12 @@ const Dashboard = () => {
                         sortedCoins.map((coin) => <CoinCard key={coin.id} {...coin} />)
                     }
                 </div>
-                <BitcoinGraph />
+                <BitcoinGraph  />
             </div>
             <div className="live-market-transactions">
                 <div>
                     <h3>Live Market</h3>
-                    <LiveMarket data={sortedCoins} />
+                    <LiveMarket sortedCoins={sortedCoins} />
                 </div>
                 <div>
                     <h3>Transactions</h3>

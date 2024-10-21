9a006c9d9c59a9d1793af4600d8ad249e70cc6da
Refilwe06 <refilwemotseo2@gmail.com>
2024-10-21 10:19:05 +0200

    feat: added app routing

--- cryptx-frontend/src/App.js
+++ cryptx-frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Login from './pages/Login/Login';
 import Overview from './pages/Overview/Overview';
-
+import Register from './pages/Register/Register';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <Overview />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='/overview' element={<Overview />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
